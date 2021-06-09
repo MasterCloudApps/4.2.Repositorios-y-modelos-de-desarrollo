3ff8b4410ee4fb8e9424b4b3ebfe9d939dda6d21
Patxi Gortázar <patxi.gortazar@gmail.com>
2021-06-09 19:20:45 +0200

    Add findByTitle to repository

--- tbd/ejemplo2/src/main/java/es/urjc/code/daw/library/book/BookRepository.java
+++ tbd/ejemplo2/src/main/java/es/urjc/code/daw/library/book/BookRepository.java
@@ -1,7 +1,11 @@
 package es.urjc.code.daw.library.book;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+	
+	List<Book> findByTitle(String title);
 
 }
